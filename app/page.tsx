3971e71141ecc61e6d0aa79c32d5de9995ea4e4e
hiigorsilva <higorscontato@gmail.com>
2024-08-06 18:06:52 -0300

    feat: add booking item

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,21 @@ import { Header } from "./_components/Header"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
 
 const Home = () => {
   return (
     <div>
-      {/* Header */}
+      {/* HEADER */}
       <Header />
       <div className="p-5">
+        {/* SAUDAÇÕES */}
         <h2 className="text-xl font-semibold">Olá, Higor!</h2>
         <p>Segunda-feira, 05 de agosto.</p>
 
+        {/* BUSCA */}
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Buscar barbearias" />
           <Button>
@@ -20,6 +25,7 @@ const Home = () => {
           </Button>
         </div>
 
+        {/* BANNER */}
         <div className="relative mt-6 h-40 w-full overflow-hidden rounded-xl">
           <Image
             src="/banner-01.svg"
@@ -28,6 +34,32 @@ const Home = () => {
             fill
           />
         </div>
+
+        <h2>Agendamento</h2>
+
+        {/* AGENDAMENTO */}
+        <Card className="mt-6">
+          <CardContent className="flex justify-between px-5 py-0">
+            {/* ESQUERDA */}
+            <div className="flex flex-col gap-2 py-5">
+              <Badge className="w-fit">Confirmado</Badge>
+              <h3 className="font-semibold">Corte de Cabelo</h3>
+              <div className="flex items-center gap-2">
+                <Avatar>
+                  <AvatarImage src="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png" />
+                </Avatar>
+                <p className="text-sm">Barbearia FSW</p>
+              </div>
+            </div>
+
+            {/* DIREITA */}
+            <div className="flex flex-col items-center justify-center border-l border-solid pl-5">
+              <p className="text-sm">Agosto</p>
+              <p className="text-2xl">06</p>
+              <p className="text-sm">20:00</p>
+            </div>
+          </CardContent>
+        </Card>
       </div>
     </div>
   )
