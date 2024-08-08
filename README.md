000d939384ec99958fa9f2a4dc8ad728d39fa819
hiigorsilva <higorscontato@gmail.com>
2024-08-08 18:51:35 -0300

    chore: add next auth setup

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Barber App
+Continuar [video aula](https://youtu.be/aIWSXdFggJQ?t=1142) em: 19min 00seg 
--- app/_components/SidebarSheet.tsx
+++ app/_components/SidebarSheet.tsx
@@ -1,10 +1,17 @@
-import { CalendarIcon, HomeIcon, LogOutIcon } from "lucide-react"
+import { CalendarIcon, HomeIcon, LogInIcon, LogOutIcon } from "lucide-react"
 import { Button } from "./ui/button"
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from "./ui/sheet"
-import { Avatar, AvatarImage } from "./ui/avatar"
 import Link from "next/link"
 import { quicksSearchOptions } from "../_constants/search"
 import Image from "next/image"
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog"
 
 export const SidebarSheet = () => {
   return (
@@ -15,8 +22,35 @@ export const SidebarSheet = () => {
       </SheetHeader>
 
       {/* AVATAR */}
-      <div className="flex items-center gap-3 border-b border-solid pb-5">
-        <Avatar>
+      <div className="flex items-center justify-between gap-3 border-b border-solid pb-5">
+        <h2 className="font-semibold">Olá, faça seu login!</h2>
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="icon">
+              <LogInIcon />
+            </Button>
+          </DialogTrigger>
+
+          <DialogContent className="w-[90%] space-y-3 rounded-xl">
+            <DialogHeader className="space-y-2">
+              <DialogTitle>Faça login na plataforma</DialogTitle>
+              <DialogDescription>
+                Conecte-se usando sua conta do Google
+              </DialogDescription>
+            </DialogHeader>
+            <Button variant="outline" className="gap-2 font-semibold">
+              <Image
+                src="/google.svg"
+                alt="Fazer login com o Google"
+                width={18}
+                height={18}
+              />
+              Google
+            </Button>
+          </DialogContent>
+        </Dialog>
+
+        {/* <Avatar>
           <AvatarImage
             src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?q=80&w=580&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt="Foto de perfil"
@@ -28,7 +62,7 @@ export const SidebarSheet = () => {
         <div>
           <p className="font-semibold">Higor Silva</p>
           <p className="text-sm text-zinc-400">higorsilva@gmail.com</p>
-        </div>
+        </div> */}
       </div>
 
       {/* INICIO E AGENDAMENTO */}
